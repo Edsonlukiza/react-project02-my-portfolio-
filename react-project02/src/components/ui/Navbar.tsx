d5efcc1acd3caab7c740ce0876d95d7924345676
edson <edsonlukiza@gmail.com>
2026-05-05 09:44:56 +0300

    navbar modification

--- react-project02/src/components/ui/Navbar.tsx
+++ react-project02/src/components/ui/Navbar.tsx
@@ -12,16 +12,15 @@ function Navbar() {
                 <a href="/" className="nav-logo">
                     MyPortfolio
                 </a>
-
-                {/* Desktop Menu */}
-                <div className="nav-menu">
-                    <a href="/" className="nav-link">Home</a>
-                    <a href="/projects" className="nav-link">Projects</a>
-                    <a href="/about" className="nav-link">About</a>
-                    <a href="/contact" className="nav-link">Contact</a>
+                <div className="exten">
+                    <div className="nav-menu">
+                        <a href="/" className="nav-link">Home</a>
+                        <a href="/projects" className="nav-link">Projects</a>
+                        <a href="/about" className="nav-link">About</a>
+                        <a href="/contact" className="nav-link">Contact</a>
+                    </div>
                 </div>
 
-                {/* Mobile Menu Button */}
                 <button
                     className="mobile-menu-btn"
                     onClick={() => setIsMenuOpen(!isMenuOpen)}
@@ -30,7 +29,7 @@ function Navbar() {
                 </button>
             </div>
 
-            {/* Mobile Dropdown Menu */}
+
             {isMenuOpen && (
                 <div className="mobile-menu">
                     <a href="/">Home</a>
